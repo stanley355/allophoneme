7e4d7aeb6455438adb294f1f68c1a107f94f4ead
Stanley Winata <winatastanley355@gmail.com>
2023-02-07 10:46:01 +0700

    Add calamine cargo

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ mod cli;
 fn main() {
     cli::Cli::welcome_text();
     cli::Cli::welcome_input();
-    // let args: Vec<String> = env::args().collect();
 
-    // let query = &args[0];
-    // println!("{}", query);
+    // let file_path = "../poem.txt";
+    // let contents = fs::read_to_string(file_path)
+    //     .expect("Should have been able to read the file");
 
+    // println!("With text:\n{contents}");
 }
