0b6b7bf49a101b59c9b8f1c7d2d67540879ee074
Stanley Winata <winatastanley355@gmail.com>
2023-02-07 10:49:00 +0700

    Some edit

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,4 @@ fn main() {
     cli::Cli::welcome_input();
 
     // let file_path = "../poem.txt";
-    // let contents = fs::read_to_string(file_path)
-    //     .expect("Should have been able to read the file");
-
-    // println!("With text:\n{contents}");
 }
