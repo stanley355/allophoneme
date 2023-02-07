1429699b385e173d268251f05ec63235fd7c6c47
Stanley Winata <winatastanley355@gmail.com>
2023-02-07 10:32:33 +0700

    Create receive cli input

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,1 +1,33 @@
-pub mod welcome;
+use std::io;
+
+pub struct Cli;
+
+impl Cli {
+    pub fn welcome_text() {
+        const WELCOME_TEXTS: [&str; 4] = [
+            "Welcome Boss!",
+            "How can I help you?",
+            "1. Read Excel",
+            "2. Migrate Excel",
+        ];
+        for text in WELCOME_TEXTS {
+            println!("{}", text);
+        }
+    }
+
+    pub fn welcome_input() {
+        println!("Enter a number:");
+        let mut input: String = String::new();
+        io::stdin().read_line(&mut input).unwrap(); //Read user input
+
+        let input_res = input.trim().parse::<i32>();
+
+        match input_res {
+            Ok(res) => println!("Your input is {}", res),
+            Err(_) => {
+                println!("Input must be number!");
+                Self::welcome_input();
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 mod cli;
 
 fn main() {
-    cli::welcome::welcome_text();
+    cli::Cli::welcome_text();
+    cli::Cli::welcome_input();
     // let args: Vec<String> = env::args().collect();
 
     // let query = &args[0];
     // println!("{}", query);
+
 }
