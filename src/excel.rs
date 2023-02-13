e88bedfbd2b07f2292a1b8185af6e5983244eac4
Stanley Winata <winatastanley355@gmail.com>
2023-02-13 08:40:40 +0700

    Enter number for file path

--- src/excel.rs
+++ src/excel.rs
@@ -18,20 +18,26 @@ impl Excel {
 
     pub fn read_financial_report_cli() {
         println!("You chose 1. Read Financial Report");
-        println!("Excel file listed on parent dir:");
-        Excel::find_excel_file_in_parent_dir();
-        let path = Cli::request_input("Enter file path:");
+        println!("Which excel file you want me to read?");
+        let excel_files = Excel::find_excel_file_in_parent_dir();
+        for (i, excel) in excel_files.iter().enumerate() {
+            println!("{}. {}", i + 1, excel);
+        }
+        let path = Cli::request_input("Enter a number:");
     }
 
-    pub fn find_excel_file_in_parent_dir() {
+    pub fn find_excel_file_in_parent_dir() -> Vec<String> {
+        let mut excel_files: Vec<String>= Vec::new();
         let parent_dir = fs::read_dir("../").unwrap();
-        for (i, dir_entry) in parent_dir.into_iter().enumerate() {
+        for dir_entry in parent_dir {
             let file_path = dir_entry.unwrap().path().to_string_lossy().to_string();
             let is_excel_entry = &file_path.contains("xlsx");
             if *is_excel_entry {
-                println!("{}. {}", i, file_path);
+                excel_files.push(file_path);
             }
         }
+
+        excel_files
     }
 
     pub fn read(self) {
