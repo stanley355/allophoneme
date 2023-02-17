8b04af63ca27675f2a22c00f097c4bcd355b34f7
Stanley Winata <winatastanley355@gmail.com>
2023-02-17 09:05:49 +0700

    Comment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 mod cli;
 mod excel;
 mod database;
+use tokio::runtime::Runtime;
 
 fn main() {
     cli::Cli::start_menu();
+    // Create a new Tokio runtime
+    // let rt = Runtime::new().unwrap();
+
+    // Run your async task within the Tokio runtime
+    // rt.block_on(async {
+    //     database::Database::create_pg_pool().await;
+    // });
 }
