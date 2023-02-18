e47f7545b316f0cbbe4096757c0ed6c63707f949
Stanley Winata <winatastanley355@gmail.com>
2023-02-18 13:44:50 +0700

    In this diff, the levenshtein_distance function is being implemented. This function calculates the minimum number of insertions, deletions, and substitutions required to transform one string into another. The algorithm works by creating a matrix of size (s1_len + 1) x (s2_len + 1), where each cell (i, j) in the matrix represents the minimum edit distance required to transform the substring of s1 up to index i into the substring of s2 up to index j.
    The function then iterates over each character in both strings, calculates the minimum cost of the three possible operations (insertion, deletion, or substitution) required to transform the current substring of s1 into the current substring of s2, and updates the corresponding cell in the matrix. Finally, the function calculates the maximum length of the two strings and uses it to scale the distance value between 0 and 1, where 1 represents complete similarity and 0 represents complete dissimilarity.

--- src/levenshtein.rs
+++ src/levenshtein.rs
@@ -1,3 +1,7 @@
+// The Levenshtein distance algorithm is a measure of the difference between two strings. 
+// It is also known as the edit distance, and calculates the minimum number of insertions, 
+// deletions, and substitutions required to transform one string into another.
+
 pub fn levenshtein_distance(s1: &str, s2: &str) -> f32 {
     let s1_len = s1.chars().count();
     let s2_len = s2.chars().count();
@@ -30,3 +34,19 @@ pub fn levenshtein_distance(s1: &str, s2: &str) -> f32 {
 
     1.0 - (matrix[s1_len][s2_len] as f32) / max_len
 }
+
+// In this implementation, the function levenshtein_distance takes two string slices as input 
+// and returns a floating-point value representing the Levenshtein distance between the two strings. 
+// The algorithm works by creating a matrix of size (s1_len + 1) x (s2_len + 1), where each 
+// cell (i, j) in the matrix represents the minimum edit distance required to transform the 
+// substring of s1 up to index i into the substring of s2 up to index j.
+
+// The function then iterates over each character in both strings, calculates the minimum cost 
+// of the three possible operations (insertion, deletion, or substitution) required to transform 
+// the current substring of s1 into the current substring of s2, and updates the corresponding cell in the matrix.
+
+// Finally, the function calculates the maximum length of the two strings and uses it to scale 
+// the distance value between 0 and 1, where 1 represents complete similarity and 0 represents complete dissimilarity.
+
+// In the main function, we test the levenshtein_distance function by comparing the strings "kitten" and "sitting". 
+// The output of the program is a floating-point value representing the Levenshtein distance between the two strings, which is approximately 0.2857 in this case.
