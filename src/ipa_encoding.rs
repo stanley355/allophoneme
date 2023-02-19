23585d413968018bca2cdfecc7e5f52c1e6853fa
Stanley Winata <winatastanley355@gmail.com>
2023-02-19 10:40:15 +0700

    The git diff is for the file ipa_encoding.rs. The diff shows the changes made to the file, with the addition of new lines indicated in green and the removal of lines indicated in red.
    The changes made in this diff add new entries to the IPA_ENCODING_LIST constant, which is a list of IpaEncoding objects. The new entries added are for the sounds ɛ, æ, ʌ, ə, ɚ, u, ʊ, o, ɔ, ɑ, ɑɪ, ɑʊ, and ɔɪ.

--- src/ipa_encoding.rs
+++ src/ipa_encoding.rs
@@ -14,48 +14,44 @@ impl IpaEncoding {
 }
 
 pub const IPA_ENCODING_LIST: [IpaEncoding] = [
-        ["i", "v1"]
-     ["ɪ", "v2"]
-     ["e", "v3"]
-     ["ɛ", "v4"]
-     ["æ", "v5"]
-     ["ʌ", "v6"]
-     ["ə", "v7"]
-     ["ɚ", "v8"]
-     ["u", "v9"]
-     ["ʊ", "v10"]
-     ["o", "v11"]
-     ["ɔ", "v12"]
-     ["ɑ", "v13"]
-     ["ɑɪ", "v14"]
-     ["ɑʊ", "v15"]
-     ["ɔɪ", "v16"]
-     ["p", "c1"]
-     ["b", "c2"]
-     ["d", "c3"]
-     ["t", "c4"]
-     ["k", "c5"]
-     ["g", "c6"]
-     ["f", "c7"]
-     ["v", "c8"]
-     ["ɵ", "c9"]
-     ["ð", "c10"]
-     ["s", "c11"]
-     ["z", "c12"]
-     ["ʃ", "c13"]
-     ["ʒ", "c14"]
-     ["h", "c15"]
-     ["ʧ", "c16"]
-     ["ʤ", "c17"]
-     ["m", "c18"]
-     ["n", "c19"]
-     ["ŋ", "c20"]
-     ["l", "c21"]
-     ["r", "c22"]
-     ["w", "c23"]
-     ["j", "c24"]
     IpaEncoding::new("i", "v1"),
     IpaEncoding::new("ɪ", "v2"),
     IpaEncoding::new("e", "v3"),
-    IpaEncoding::new("", encoding),
+    IpaEncoding::new("ɛ", "v4"),
+    IpaEncoding::new("æ", "v5"),
+    IpaEncoding::new("ʌ", "v6"),
+    IpaEncoding::new("ə", "v7"),
+    IpaEncoding::new("ɚ", "v8"),
+    IpaEncoding::new("u", "v9"),
+    IpaEncoding::new("ʊ", "v10"),
+    IpaEncoding::new("o", "v11"),
+    IpaEncoding::new("ɔ", "v12"),
+    IpaEncoding::new("ɑ", "v13"),
+    IpaEncoding::new("ɑɪ", "v14"),
+    IpaEncoding::new("ɑʊ", "v15"),
+    IpaEncoding::new("ɔɪ", "v16"),
+    IpaEncoding::new("p", "c1"),
+    IpaEncoding::new("b", "c2"),
+    IpaEncoding::new("d", "c3"),
+    IpaEncoding::new("t", "c4"),
+    IpaEncoding::new("k", "c5"),
+    IpaEncoding::new("g", "c6"),
+    IpaEncoding::new("f", "c7"),
+    IpaEncoding::new("v", "c8"),
+    IpaEncoding::new("ɵ", "c9"),
+    IpaEncoding::new("ð", "c10"),
+    IpaEncoding::new("s", "c11"),
+    IpaEncoding::new("z", "c12"),
+    IpaEncoding::new("ʃ", "c13"),
+    IpaEncoding::new("ʒ", "c14"),
+    IpaEncoding::new("h", "c15"),
+    IpaEncoding::new("ʧ", "c16"),
+    IpaEncoding::new("ʤ", "c17"),
+    IpaEncoding::new("m", "c18"),
+    IpaEncoding::new("n", "c19"),
+    IpaEncoding::new("ŋ", "c20"),
+    IpaEncoding::new("l", "c21"),
+    IpaEncoding::new("r", "c22"),
+    IpaEncoding::new("w", "c23"),
+    IpaEncoding::new("j", "c24"),
 ];
