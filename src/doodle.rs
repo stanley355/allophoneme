5e2abeedf39f7bc44a0bf521ffeed8d55253a484
Stanley Winata <winatastanley355@gmail.com>
2023-02-20 09:44:49 +0700

    The git diff is for the src/main.rs file. The index 97c6acf..8bc4af2 100644 line indicates the two versions of the file that are being compared. The lines that begin with --- and +++ show the lines that have been added or removed. In this case, a line has been added at line 4. This line contains a mod declaration for a new module, constant.

---
+++ src/doodle.rs
@@ -0,0 +1,11 @@
+
+#[derive(Debug, Clone)]
+pub struct Doodle {
+    pub word: String,
+    pub ipa: String,
+    pub ipa_encoded: String,
+}
+
+impl Doodle {
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod excel;
 mod ipa_encoding_pair;
 mod levenshtein;
 mod word_ipa_pair;
+mod constant;
 
 fn main() {
     cli::Cli::start_menu();
