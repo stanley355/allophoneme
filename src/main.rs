f1e4cdec906df5e4c79fba5830c9effa970f5feb
Stanley Winata <winatastanley355@gmail.com>
2023-03-16 13:19:37 +0700

    Add gtk

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,28 @@ mod constant;
 mod doodle;
 mod doodle_similarity;
 
-fn main() {
-    cli::Cli::start_menu();
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
+fn main() -> glib::ExitCode {
+    // cli::Cli::start_menu();
+    let app = Application::builder()
+        .application_id("org.example.HelloWorld")
+        .build();
+
+    app.connect_activate(|app| {
+        // We create the main window.
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("Hello, World!")
+            .build();
+
+        // Show the window.
+        window.show();
+    });
+
+    app.run()
 }
