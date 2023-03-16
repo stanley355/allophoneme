d692c9e4952446285d3bf41eebb3cd5833e43a5b
Stanley Winata <winatastanley355@gmail.com>
2023-03-16 13:36:51 +0700

    First gtk

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,28 @@
 // mod doodle_similarity;
 
 use gtk::prelude::*;
-use gtk::{glib, Application};
+use gtk::{glib, Application, ApplicationWindow};
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld1";
+const APP_ID: &str = "org.gtk_rs.HelloWorld2";
 
 fn main() -> glib::ExitCode {
-    // cli::Cli::start_menu();
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
     // Run the application
     app.run()
+}
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .build();
+
+    // Present window
+    window.present();
 }
